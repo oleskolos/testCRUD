fabca5273167b8100f6329e9fe444b007ff6d113
oleskolos <oleskolos03@gmail.com>
2023-11-12 00:55:48 +0200

    delete action

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,14 @@
         <th scope="row">{{ $user->id }}</th>
         <td><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></td>
         <td><a href="{{ route('users.show', $user) }}">{{ $user->email }}</a></td>
-        <td><a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a></td>
+        <td>
+            <form method="POST" action="{{ route('users.destroy', $user) }}">
+                <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">Delete</button>
+            </form>
+        </td>
         </tr>
         @endforeach
     </tbody>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -16,6 +16,11 @@
   </ul>
 </div>
 
-<a type="button" class="btn btn-warning mt-3" href="{{ route('users.edit', $user) }}">Edit</a>
+<form method="POST" action="{{ route('users.destroy', $user) }}" class="mt-3">
+    <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
+        @csrf
+        @method('DELETE')
+    <button class="btn btn-danger" type="submit">Delete</button>
+</form>
 
 @endsection
